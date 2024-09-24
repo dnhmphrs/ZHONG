d1358eca79637576b837da92047b922220199af7
dnhmphrs <dan@aufbau.io>
2024-09-24 21:18:10 +0100

    begin user pages

--- src/lib/components/user/userList.svelte
+++ src/lib/components/user/userList.svelte
@@ -1,12 +1,21 @@
 <script>
-  let users = Array.from({ length: 30 }, (_, i) => `User ${i + 1}`);
+  import { goto } from '$app/navigation';
+
+  // Dummy user data
+  let users = Array.from({ length: 30 }, (_, i) => ({ id: i + 1, name: `User ${i + 1}` }));
+
+  function selectUser(user) {
+    goto(`/user/${user.id}`);
+  }
 </script>
 
 
 <div class="list">
   <ul>
     {#each users as user}
-      <li>{user}</li>
+      <li on:click={() => selectUser(user)} on:keydown={() => selectUser(user)}>
+        {user.name}
+      </li>
     {/each}
   </ul>
 </div>
@@ -27,7 +36,12 @@
   }
 
   li {
+    cursor: pointer;
     padding: 8px 0;
     border-top: 1px solid var(--primary-50);
+  }
+
+  li:hover, li:active {
+    color: var(--primary-50);
   }
 </style>
---
+++ src/routes/user/[id].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  export async function load({ params }) {
+    const { id } = params; // Grab the user ID from the URL
+    // You can fetch user details here based on the ID, for now let's just return it
+    return { id };
+  }
+</script>
+
+<script>
+  export let id; // id will come from the load function
+</script>
+
+<div>
+  <h1>User ID: {id}</h1>
+  <!-- User-specific content can be displayed here -->
+</div>
