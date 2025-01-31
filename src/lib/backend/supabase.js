64d5eab9c0218d37fa4c79d58db04dc110c144d9
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-01-31 13:26:17 +0200

    push

--- src/lib/backend/supabase.js
+++ src/lib/backend/supabase.js
@@ -4,3 +4,5 @@ export const supabase = createClient(
 	'https://lllppvforvdylpugodve.supabase.co',
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbHBwdmZvcnZkeWxwdWdvZHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyOTU3ODIsImV4cCI6MjAyNDg3MTc4Mn0.m1algJr29uo1PqMWoNxnVYUv2O_5QSU9iFrYJrqCCWk'
 );
+
+// tmp
